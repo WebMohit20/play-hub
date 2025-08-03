dac0deda048081aca5fa29ca87c9b0ebef1478ca
WebMohit <2003mohitkhemani@gmail.com>
2025-08-03 12:29:18 +0530

    connect to mongo atlas

---
+++ src/server.js
@@ -0,0 +1,15 @@
+import dotenv from "dotenv";
+import express from "express";
+import connectDB from "./db/index.js";
+
+dotenv.config();
+
+const app = express();
+
+connectDB();
+
+
+
+app.listen(process.env.PORT,()=>{
+    console.log("Server is running on port: ",process.env.PORT);
+})
